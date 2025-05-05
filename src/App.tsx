4d73589bf4738397d24f40272f2fe6c98667fbf7
Harry Parkes <harrydparkes@proton.me>
2025-05-05 20:35:26 +1200

    Feature: Added appbar, and fixed codemirror to the left half of the screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import CodeMirror from '@uiw/react-codemirror';
+import Toolbar from '@mui/material/Toolbar';
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import { swimdsl } from 'codemirror-lang-swimdsl';
 
 import './App.css';
@@ -30,17 +34,30 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <CodeMirror
-        value={value}
-        height="850px"
-        width="900px"
-        theme={prefersDarkMode ? "dark" : "light"}
-        extensions={[swimdsl()]}
-        onChange={onChange}
-      />;
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div">
+            App Bar
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{ display: "flex", height: "100vh" }}>
+        <Box sx={{ width: "50%", height: "100%" }}>
+          <CodeMirror
+            value={value}
+            height="100vh"
+            width="100%"
+            theme={prefersDarkMode ? "dark" : "light"}
+            extensions={[swimdsl()]}
+            onChange={onChange}
+          />
+        </Box>
+        <Box sx={{ width: "50%", height: "100%" }}>
+          Right Half
+        </Box>
+      </Box>
     </ThemeProvider>
   );
-
 }
 
 export default App;
