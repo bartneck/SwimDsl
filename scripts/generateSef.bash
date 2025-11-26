341b35e444581686b92c6d8f4e244293b4a4f1d7
Harrison Parkes <git@hparkes.com>
2025-11-26 19:39:26 +1000

    Fix: Updated generate SEF script to work on MacOS/BSD

--- scripts/generateSef.bash
+++ scripts/generateSef.bash
@@ -24,13 +24,20 @@ sef_file_path="./${STATIC_HTTP_DIR}/${SEF_FILE_NAME}"
 static_http_uri="file://$(cd "${STATIC_HTTP_DIR}" && pwd -P)"
 
 # Download the latest version of the swiML XSL transformation schema
-curl --fail ${MASTER_XSL_URL} >"${xsl_file_path}"
+if [ ! -f "${xsl_file_path}" ]; then
+    curl --fail ${MASTER_XSL_URL} >"${xsl_file_path}"
+fi
 
 # Compile it into a SEF JSON file
-node node_modules/xslt3/xslt3.js -xsl:"${xsl_file_path}" -export:"${sef_file_path}" -t -ns:##html5 -nogo
+if [ ! -f "${sef_file_path}" ]; then
+    node node_modules/xslt3/xslt3.js -xsl:"${xsl_file_path}" -export:"${sef_file_path}" -t -ns:##html5 -nogo
+fi
 
 # Replace references to the XSL file with the hosted instance
-sed --in-place --expression="s&${static_http_uri}&${deployed_base_url}&g" "${sef_file_path}"
+temp_file=$(mktemp "${sef_file_path}.XXXXXX")
+sed "s&${static_http_uri}&${deployed_base_url}&g" "${sef_file_path}" >"${temp_file}"
+mv "${temp_file}" "${sef_file_path}"
+echo "Replaced occurance of ${static_http_uri} with ${deployed_base_url}"
 
 # Calculate what the checksum should be for the updated sef json
 new_checksum=$(./scripts/computeChecksum.ts "${sef_file_path}")
@@ -43,3 +50,4 @@ jq --arg cs "${new_checksum}" '.["\u03A3"] = $cs' "${sef_file_path}" >"${temp_fi
 
 # Overwrite the original file with the temporary file
 mv "${temp_file}" "${sef_file_path}"
+echo "Updated checksum"
