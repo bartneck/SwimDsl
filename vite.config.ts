fbf41170eb25fe03c303f28a145d732e3ca63cc2
Harrison Parkes <git@hparkes.com>
2025-11-30 10:02:41 +1000

    Chore: Splitting node modules into different chunks to hopefully reduce loading time

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes("node_modules")) {
+            return id.split("node_modules/")[1].split("/")[0];
+          }
+        },
+      },
+    },
+  },
 });
