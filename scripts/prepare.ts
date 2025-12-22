747bbeb2674f9847a90c5be3735cc2ae23a6b7a7
Harrison Parkes <git@hparkes.com>
2025-12-22 10:47:03 +1000

    Refactor: Changed check against file path with CI environment variable

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -2,11 +2,10 @@ import fetchFonts from "./fetchFonts.ts";
 import generateSef from "./src/generateSef.ts";
 
 async function main(): Promise<void> {
-  const scriptPath = process.argv[1];
-
-  const deployedBaseUrl = scriptPath.startsWith("/home/runner")
-    ? "https://bartneck.github.io/SwimDsl"
-    : "http://localhost:5173";
+  const deployedBaseUrl =
+    process.env.CI === "true"
+      ? "https://bartneck.github.io/SwimDsl"
+      : "http://localhost:5173";
 
   const sef = generateSef(deployedBaseUrl).then(() => {
     console.log(`Wrote SEF file`);
